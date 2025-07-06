530efa32dd94f526ca8afbcf06dbdc7994f4ad34
EDUARDO CZAPLEWSKI <edczaplewski@gmail.com>
2025-07-06 17:44:19 +0000

    Hay que poner los botones de la agenda una vez seleccionada qué se ponga

--- src/components/ui/calendar.tsx
+++ src/components/ui/calendar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -43,7 +44,7 @@ function Calendar({
         ),
         day_range_end: "day-range-end",
         day_selected:
-          "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
+          "bg-green-600 text-primary-foreground hover:bg-green-600/90 focus:bg-green-600 focus:text-primary-foreground",
         day_today: "bg-accent text-accent-foreground",
         day_outside:
           "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
