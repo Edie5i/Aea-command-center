6bb85eac4217990316f7ed770d977231ad778007
EDUARDO CZAPLEWSKI <edczaplewski@gmail.com>
2025-07-08 20:20:47 +0000

    <script src="//code.tidio.co/eqlxaq6u3e6jdbcqtoo2saunntgiip04.js" async>

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { cn } from "@/lib/utils";
 import { Toaster } from "@/components/ui/toaster";
 import "./globals.css";
 import { FloatingWhatsappButton } from "@/components/floating-whatsapp-button";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "AEA - Autoescuela Americana",
@@ -33,6 +34,7 @@ export default function RootLayout({
         {children}
         <Toaster />
         <FloatingWhatsappButton />
+        <Script src="//code.tidio.co/eqlxaq6u3e6jdbcqtoo2saunntgiip04.js" async />
       </body>
     </html>
   );
