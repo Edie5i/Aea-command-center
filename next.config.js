d3b08f77dc68d9e26984a3694ac7afb8b8658953
EDUARDO CZAPLEWSKI <edczaplewski@gmail.com>
2025-09-03 15:24:22 +0000

    Podemos revisar que todo este en orden para actualizar y poner nota de c

--- next.config.js
+++ next.config.js
@@ -33,6 +33,12 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.autoescuelaamericana.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
