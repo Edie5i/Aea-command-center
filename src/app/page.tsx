1e99ba4102094d691d4cadfcae362b1a0ba6efb7
EDUARDO CZAPLEWSKI <edczaplewski@gmail.com>
2025-11-26 04:24:47 +0000

    Marca error de not found Google maps

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -157,7 +157,7 @@ export default function Home() {
                 <div className="w-full aspect-video">
                     <iframe
                         className="w-full h-full"
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.9649175317183!2d-99.16635672568777!3d19.41400644053912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3b16555555%3A0x1c80842f1f13380b!2sAuto%20Escuela%20Americana!5e0!3m2!1ses-419!2smx!4v1700000000000"
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.9649175317183!2d-99.16635672568777!3d19.41400644053912!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1ff3b16555555%3A0x1c80842f1f13380b!2sAuto%20Escuela%20Americana!5e0!3m2!1ses-419!2smx!4v1721856191011!5m2!1ses-419!2smx"
                         style={{ border: 0 }}
                         allowFullScreen={true}
                         loading="lazy"
