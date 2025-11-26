6a27007bd9172e9098daaa935334bc1439f7d119
EDUARDO CZAPLEWSKI <edczaplewski@gmail.com>
2025-11-26 16:14:32 +0000

    revisa que si aparesca el pin de auto escuela americana y aparesca excta

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -66,7 +66,6 @@ export default function Home() {
       <div className="container px-4 sm:px-6 md:px-8 pb-8 flex flex-col items-center gap-8">
         
         <div className="w-full max-w-6xl grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-          
           <Card className="flex flex-col">
             <CardContent className="p-6 flex-grow">
               <CalendarCheck className="h-8 w-8 text-primary mb-4" />
@@ -106,7 +105,7 @@ export default function Home() {
             </div>
           </Card>
 
-          <Card className="flex flex-col">
+           <Card className="flex flex-col">
             <CardContent className="p-6 flex-grow">
               <List className="h-8 w-8 text-primary mb-4" />
               <h3 className="font-bold text-lg mb-2">Catálogo de Cursos</h3>
